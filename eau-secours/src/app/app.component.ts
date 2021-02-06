4656a0ad74480adaced57c414630591e053f5e39
William Martineau <william_martineau@hotmail.com>
2021-02-06 11:24:27 -0500

    map is working

--- eau-secours/src/app/app.component.ts
+++ eau-secours/src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Loader } from "@googlemaps/js-api-loader"
+import {} from 'googlemaps';
+
+const loader = new Loader({
+  apiKey: "AIzaSyAfowCfopxwTeabNFPuv0av4Bwc0-az0r8",
+  version: "weekly",
+});
+
+let map: google.maps.Map;
+let marker: google.maps.Marker;
+const legend = document.getElementById("legend") as HTMLElement;
+
+function initMap(): void {
+  loader.load().then(() => {
+    map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  });
+}
+
+
+function addMarker(): void {
+  loader.load().then(() => {
+    const myLatLng = { lat: -25.363, lng: 131.044 };
+
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 4,
+      center: myLatLng,
+    }
+  );
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
+
+
+  });
+}
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eau-secours';
+  constructor() { }
+
+  ngOnInit() {
+    initMap();
+    addMarker();
+    
+  }
 }
