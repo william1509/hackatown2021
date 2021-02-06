d97b0be14c9a42c8f01e399c9c7de1c5a69b2e29
Carl <corteocarl@gmail.com>
2021-02-06 18:26:19 -0500

    Better-looking star rating

--- eau-secours/src/app/components/star-rating/star-rating.component.ts
+++ eau-secours/src/app/components/star-rating/star-rating.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'star-rating',
@@ -6,9 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./star-rating.component.css']
 })
 export class StarRating {
-    public rating: number = 5;
+    @Output() private ratingChanged = new EventEmitter();
     
-    public setRating(rating: number) {
+    private starNum: number = 5;
+    private rating: number = 0;
+    public starArr = []; // Used for DOM creation
+    
+    constructor() {
+        for(let i = 0; i < this.starNum; ++i) {
+            this.starArr.push(i);
+        }
+    }
+    
+    onClick(rating: number) {
         this.rating = rating;
+        this.ratingChanged.emit(this.rating);
+    }
+    
+    isChecked(rating: number) {
+        return this.rating > (rating - 0.5);
     }
 }
