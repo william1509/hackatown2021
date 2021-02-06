5ed47def995f7b4309b61fcbe5496857e03eac01
lazureetienne <lazureetienne@gmail.com>
2021-02-06 16:05:55 -0500

    Progress on display and popup

--- eau-secours/src/app/app.module.ts
+++ eau-secours/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FountainDisplayComponent } from './components/fountain-display/fountain-display.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { FountainPopupComponent } from './components/fountain-popup/fountain-popup.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     FountainDisplayComponent,
+    FountainPopupComponent
   ],
   imports: [
     BrowserModule,
--- eau-secours/src/app/components/fountain-display/fountain-display.component.ts
+++ eau-secours/src/app/components/fountain-display/fountain-display.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { stringify } from 'querystring';
+import { MatDialog } from '@angular/material/dialog';
+import { inject } from '@angular/core/testing';
 @Component({
   selector: 'app-fountain-display',
   templateUrl: './fountain-display.component.html',
   styleUrls: ['./fountain-display.component.css']
 })
+/* Inject data here. Import at line 3. */
 export class FountainDisplayComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  fountainName : string;
+  starRating : number;
+  numberRatings : number;
+  userStars : number;
+/*  constructor(@Inject(String)fountainName : string, @Inject(Number)starRating : number, @Inject(Number)numberRatings : number, @Inject(Number)userStars : number) {
+    this.fountainName = fountainName;
+    this.starRating = starRating;
+    this.numberRatings = numberRatings;
+    this.userStars = userStars;
+   }
+  */ngOnInit(): void {
   }
 
 }
